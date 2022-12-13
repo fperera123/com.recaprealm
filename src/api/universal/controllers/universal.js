0c641dfe2d9919264b4834f9e12f7cc0e6db8ccd
fperera123 <mrlive119@ymail.com>
2022-12-14 04:44:31 +0530

    init repo

---
+++ src/api/universal/controllers/universal.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * universal controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::universal.universal');
+
